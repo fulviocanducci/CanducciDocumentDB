d5e4fb02b831a090ea5958d560d85d1c848c2a5b
fulviocanducci <fulviocanducci@hotmail.com>
2017-02-19 22:16:04 -0300

    First

---
+++ Canducci.DocumentDB/ConnectionDocumentDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Documents.Client;
+using System;
+namespace Canducci.DocumentDB
+{
+    public sealed class ConnectionDocumentDB: IDisposable
+    {        
+        public string DatabaseName { get; private set; } = "Todo";
+        public DocumentClient Client { get; private set; }
+
+        public ConnectionDocumentDB(
+            string url = "https://localhost:8081/",
+            string key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", 
+            string database = "Todo")
+        {
+            Client = new DocumentClient(new Uri(url),
+                key, 
+                new ConnectionPolicy { EnableEndpointDiscovery = false });
+            DatabaseName = database;         
+        }
+        public void Dispose()
+        {
+            if (Client != null)
+            {
+                Client.Dispose();
+                Client = null;
+            }
+        }
+    }
+}
---
+++ Canducci.DocumentDB/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Canducci.DocumentDB
+{
+    public interface IRepository<T>
+        where T : class, new()
+    {
+        Task<T> InsertAsync(T document);
+        Task<ResourceResponse<Document>> UpdateAsync(T document, string id);
+        Task<ResourceResponse<Document>> DeleteAsync(string id);
+        Task<T> FindAsync(string id);
+        Task<IEnumerable<T>> AllAsync(Expression<Func<T, bool>> where);
+        Task<IEnumerable<T>> AllAsync<TKey>(Expression<Func<T, bool>> where, Expression<Func<T, TKey>> orderBy);
+        Task<IEnumerable<TDocument>> AllAsync<TKey, TDocument>(Expression<Func<T, bool>> where, Expression<Func<T, TKey>> orderBy, Expression<Func<T, TDocument>> select);
+        IOrderedQueryable Query();
+    }
+}
